691a7e97948285118c7ff07f17f3833072f2c0c6
Marco Firriolo <marcofirriolo@gmail.com>
2025-11-02 18:35:58 +0100

    Main component

---
+++ DaiGiochi/src/components/daiGioca/daiGioca.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+interface daiGiocaProps {}
+
+const DaiGioca: FC<daiGiocaProps> = () => (
+ <div>
+    daiGioca Component
+ </div>
+);
+
+export default DaiGioca;
